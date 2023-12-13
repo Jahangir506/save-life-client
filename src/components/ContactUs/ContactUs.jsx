ae71f739fdd9cb21869014d16ef7ce8878659ced
Jahangir506 <zahangirkhan506@gmail.com>
2023-12-13 16:53:36 +0600

    contact section message send to gamil

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,13 +1,42 @@
 import { FaClock, FaLocationDot, FaPhone } from "react-icons/fa6";
 import TitleSection from "../shared/TitleSection/TitleSection";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
+
 const ContactUs = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_k2feorh",
+        "template_ybrl8rj",
+        form.current,
+        "uNhXKQ4NTgua0hoMz"
+      )
+      .then(
+        (result) => {
+          Swal.fire("Send Message!");
+          console.log(result.text);
+          form.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <>
       <div className="hero mt-20">
         <div className="flex flex-col w-24 lg:w-full border-opacity-50 pb-8">
-          <div className="divider w-w-[200px] lg:w-[900px] divide
-           mx-auto ">
-            <TitleSection heading={"Contact Us"}/>
+          <div
+            className="divider w-w-[200px] lg:w-[900px] divide
+           mx-auto "
+          >
+            <TitleSection heading={"Contact Us"} />
           </div>
           <div className="hero-content gap-8 p-12 lg:w-full flex-col lg:flex-row">
             <div className="text-center w-1/2 lg:text-left bg-red-400 text-white">
@@ -27,7 +56,8 @@ const ContactUs = () => {
                   </p>
                   <p className="flex items-center">
                     <FaLocationDot className="mr-3 text-white" />
-                    Location : <span className="ml-7">Tangail, Dhaka, Bangladesh</span>
+                    Location :{" "}
+                    <span className="ml-7">Tangail, Dhaka, Bangladesh</span>
                   </p>
                   <p className="flex items-center">
                     <FaClock className="mr-3 text-white" />
@@ -37,38 +67,24 @@ const ContactUs = () => {
               </div>
             </div>
             <div className="card w-1/2 bg-red-400 rounded-none">
-              <form className="card-body">
+              <form ref={form} onSubmit={sendEmail} className="card-body">
                 <div className="flex justify-between mb-3">
-                  <div className="form-control">
+                  <div className="form-control w-full">
                     <input
-                      type="email"
+                      type="name"
+                      name="user_name"
                       placeholder="First Name"
-                      className="input input-bordered border-none rounded-none bg-red-200 w-full"
-                      required
-                    />
-                  </div>
-                  <div className="form-control">
-                    <input
-                      type="email"
-                      placeholder="Last Name"
                       className="input input-bordered border-none rounded-none bg-red-200 w-full"
                       required
                     />
                   </div>
                 </div>
                 <div className="flex justify-between mb-3">
-                  <div className="form-control">
+                  <div className="form-control w-full">
                     <input
                       type="email"
+                      name="user_email"
                       placeholder="Email"
-                      className="input input-bordered border-none rounded-none bg-red-200 w-full"
-                      required
-                    />
-                  </div>
-                  <div className="form-control">
-                    <input
-                      type="email"
-                      placeholder="Subject"
                       className="input input-bordered border-none rounded-none bg-red-200 w-full"
                       required
                     />
@@ -81,7 +97,9 @@ const ContactUs = () => {
                   ></textarea>
                 </div>
                 <div className="form-control mt-6">
-                  <button className="btn bg-red-100 text-black hover:bg-black hover:text-white border-none rounded-none uppercase">Login</button>
+                  <button className="btn bg-red-100 text-black hover:bg-black hover:text-white border-none rounded-none uppercase">
+                    Login
+                  </button>
                 </div>
               </form>
             </div>
